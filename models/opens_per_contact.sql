9903c8105c01020d4fe6346cc52bd783d68433d3
Roel <roel@p-tmc.com>
2022-03-13 15:33:19 +0000

    corrected dataset

--- models/opens_per_contact.sql
+++ models/opens_per_contact.sql
@@ -5,9 +5,9 @@ SELECT
     c.email_address,
     COUNT(DISTINCT o.email_id) as n_emails
 FROM 
-    b2b_data.opens o
-    LEFT JOIN b2b_data.contacts c ON o.contact_id = c.contact_id
-    LEFT JOIN b2b_data.emails e ON o.email_id = e.email_id
+    dummy_datasets.b2b_opens o
+    LEFT JOIN dummy_datasets.b2b_contacts c ON o.contact_id = c.contact_id
+    LEFT JOIN dummy_datasets.b2b_emails e ON o.email_id = e.email_id
 WHERE
     c.role = 'CIO'
 GROUP BY
